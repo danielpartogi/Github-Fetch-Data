c7621213d423455099fe48817669f0fc771c6748
daniel <danielpartogi1996@gmail.com>
2020-04-14 12:04:49 +0700

    Initial Commit

---
+++ Github Fetch Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github Fetch Data
+//
+//  Created by daniel on 14/04/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
